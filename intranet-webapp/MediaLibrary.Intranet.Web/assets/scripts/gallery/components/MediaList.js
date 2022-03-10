f64f9e58bb2d2a244927df53c1d1ba575873f166
CruzChua <cruzchua2001@gmail.com>
2022-03-10 16:25:34 +0800

    UI Changes on listview

--- intranet-webapp/MediaLibrary.Intranet.Web/assets/scripts/gallery/components/MediaList.js
+++ intranet-webapp/MediaLibrary.Intranet.Web/assets/scripts/gallery/components/MediaList.js
@@ -18,16 +18,22 @@ const Thumbnail = styled.img`
 const MediaList = ({ results }) => {
   const renderedItems = results.map((result) => {
     return (
-      <div key={result.id} className="media p-3 my-3 border rounded bg-light">
-        <ThumbnailLink href={result.link}>
-          <Thumbnail src={result.thumbnail} alt={result.name} />
-        </ThumbnailLink>
-        <div className="media-body">
-          <h5 className="mt-0">
-            <a href={result.link}>{result.name}</a>
-          </h5>
-          <p className="mb-0">{result.caption}</p>
+      <div key={result.id}>
+        <div className="media p-3 my-3">
+          <ThumbnailLink href={result.link}>
+            <Thumbnail src={result.thumbnail} alt={result.name} />
+          </ThumbnailLink>
+          <div className="media-body">
+            <h5 className="mt-0">
+              <a className="listview-text text-decoration-none text-dark" href={result.link}>{result.name}</a>
+            </h5>
+            <p className="mb-0">{result.caption}</p>
+            <p><em>Location</em></p>
+            <p className="position-relative bottom-0 end-0">By</p>
+          </div>
+        
         </div>
+        <hr />
       </div>
     )
   })
